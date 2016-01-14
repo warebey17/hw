68dbe71336bb5eec943c253aba268dd0d10a26cb
warebey17 <warebey17@gmail.com>
2016-01-14 02:04:51 +0000

    Inheritance assignment complete

---
+++ inheritance.php
@@ -0,0 +1,82 @@
+<?php
+class computer{
+
+		function sendEmail(){
+			;
+		}
+		function readCode(){
+			;
+		}
+		function surfNet(){	
+			;
+	}
+
+}
+
+	class laptop extends computer {
+
+	
+
+        // 
+		var $size;        // screen size
+		var $color;       // exterior color
+		var $model;
+		var $weight;
+		var $thinness;
+		var $sound;
+		var $material;
+		var $Ram;
+		var $processor;
+		var $resolution;   // screen display
+
+		function __construct(){
+			;
+		}
+		function playMovies(){
+			;
+		} 
+		function surfNet(){
+			;
+		}
+		function sendEmail(){
+			;
+		}
+		function readCode(){
+			;
+		}
+		function playMusic(){
+			;
+		}
+
+
+	}
+
+	class ipad extends computer {
+
+		function __construct(){
+			;
+		}	
+		function facetime(){
+		    ;
+		}
+		function snapchat(){
+			;
+		}
+		function jailbroken(){
+			;
+		}
+		function playMovies(){
+			;
+		}
+		function customize(){
+			;
+		}
+		function talkToSiri(){
+			;
+		}
+		function touchScreen(){
+			;
+		}
+			
+	}
+?>
